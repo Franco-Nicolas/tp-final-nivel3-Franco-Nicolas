3ad8c057ae9d0085f0361a54e78b94beb6a8a9a1
Franco-Nicolas <nicolas110915@outlook.es>
2024-07-14 16:36:15 -0300

    Página para ver el articulo seleccionado detalladamente.

--- Presentacion-web/ArticuloDetallado.aspx.cs
+++ Presentacion-web/ArticuloDetallado.aspx.cs
@@ -4,14 +4,34 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using dominio;
+using negocio;
 
 namespace Presentacion_web
 {
     public partial class ArticuloDetallado : System.Web.UI.Page
     {
+        public List<Articulo> ArticuloSeleccionado { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
+            try
+            {
+                string id = Request.QueryString["id"] != null ? Request.QueryString["id"].ToString() : "";
+                
+                ArticuloNegocio negocio = new ArticuloNegocio();
+                ArticuloSeleccionado = negocio.listar(id);
+                
+                if (!IsPostBack)
+                {
+                    repRepetidorDetalle.DataSource = ArticuloSeleccionado;
+                    repRepetidorDetalle.DataBind();
+                }
+            }
+            catch (Exception)
+            {
 
+                throw;
+            }
         }
     }
 }
